dc68635f2513a9eaca3b0fb2a2c0719f0912561c
timislearning <tim.mitzen@gmail.com>
2021-02-13 17:03:15 -0500

    models

--- django_inventory/app/models.py
+++ django_inventory/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Games(models.model):
+    games = models.CharField(max_length=20)
+    date_bought = models.DateTimeField('Date game purchased')
+
+class Type_Game(models.Model):
+    type = models.ForeignKey(on_delete=models.CASCADE)
+    choice = models.CharField(max_length=20)
