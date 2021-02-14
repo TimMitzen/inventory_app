6fe031df2ee4e0ca459d42d168bb87f1cdfe5562
timislearning <tim.mitzen@gmail.com>
2021-02-14 18:23:38 -0500

    migrate

--- django_inventory/app/models.py
+++ django_inventory/app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Games(models.Model):
     games = models.CharField(max_length=20)
+    number_of_games = models.IntegerChoices
+
     date_bought = models.DateTimeField('Date game purchased')
 
 class Type_Game(models.Model):
