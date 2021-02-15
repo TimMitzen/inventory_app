6240c40dd7999387f9e258e72dbe86e5f45ea6a8
timislearning <tim.mitzen@gmail.com>
2021-02-15 16:17:37 -0500

    changed fields

--- django_inventory/app/models.py
+++ django_inventory/app/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 # Create your models here.
+XBOX_ONE = "XBOX1"
+PLAYSTATION_THREE = 'PS3'
+PLAYSTATION_FOUR = 'PS4'
+NINTENDO_SWITCH = 'SWITCH'
+COMPUTER = 'PC'
+
+SYSTEM_TYPE = ((XBOX_ONE,'XBOX1'),
+               (PLAYSTATION_FOUR, 'PS4'),
+               (PLAYSTATION_THREE,'PS3'),
+               (NINTENDO_SWITCH, 'SWITCH'),
+               (COMPUTER, 'PC'))
+
 class Games(models.Model):
-    games = models.CharField(max_length=20)
-    number_of_games = models.IntegerChoices
+    game_name = models.CharField(max_length=20, blank=False, null=True)
+    game_type = models.CharField(max_length=20, blank=False, null=True)
+    game_system = models.CharField(max_length= 20,choices=SYSTEM_TYPE, null = True)
 
     date_bought = models.DateTimeField('Date game purchased')
 
