9ec99b3f94df6f657da36990a43190a8a17da2dd
timislearning <tim.mitzen@gmail.com>
2021-02-16 13:56:28 -0500

    more models

--- django_inventory/app/models.py
+++ django_inventory/app/models.py
@@ -7,18 +7,24 @@ PLAYSTATION_FOUR = 'PS4'
 NINTENDO_SWITCH = 'SWITCH'
 COMPUTER = 'PC'
 
+LIKED = 'Liked'
+OKAY = 'OK'
+NOT_LIKED = "Not Liked"
+
 SYSTEM_TYPE = ((XBOX_ONE,'XBOX1'),
                (PLAYSTATION_FOUR, 'PS4'),
                (PLAYSTATION_THREE,'PS3'),
                (NINTENDO_SWITCH, 'SWITCH'),
                (COMPUTER, 'PC'))
+GAME_OPINION = ((LIKED, 'Liked'),
+                (OKAY, 'OK'),
+                (NOT_LIKED, 'Not Liked'))
 
 class Games(models.Model):
     game_name = models.CharField(max_length=20, blank=False, null=True)
     game_type = models.CharField(max_length=20, blank=False, null=True)
     game_system = models.CharField(max_length= 20,choices=SYSTEM_TYPE, null = True)
-
-    date_bought = models.DateTimeField('Date game purchased')
+    how_much_liked = models.CharField(max_length= 20, choices=GAME_OPINION, null=True)
 
 class Type_Game(models.Model):
     type = models.ForeignKey(Games, on_delete=models.CASCADE)
